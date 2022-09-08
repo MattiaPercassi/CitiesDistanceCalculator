88e05492674a9961b9126f57882285fbb698d075
Mattia Percassi <107413452+MattiaPercassi@users.noreply.github.com>
2022-09-08 20:08:25 +0900

    tested the City class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-#include <string>
 #include "FileReader.h"
+#include "City.h"
+#include <string>
+#include <iostream>
 
 int main()
 {
-    FileReader();
+    City c1{"Bergamo", "Italia", 45.42, 09.40};
+    City c2{"Milano", "Italia", 45.28, 09.11};
+    std::cout << std::boolalpha;
+    std::cout << c1 << c2 << (c1 < c2);
+
     return 0;
 };
